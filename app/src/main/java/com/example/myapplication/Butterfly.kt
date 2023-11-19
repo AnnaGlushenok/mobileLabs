82122a299d9e911a8eb91d7ec614fdeec6079698
annas <annasan14052003@gmail.com>
2023-11-19 15:40:39 +0300

    completed

--- app/src/main/java/com/example/myapplication/Butterfly.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication
-
-data class Butterfly(
-    val name: String,
-    val image: Int,
-    val population: Int
-)
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,24 +1,60 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuItemCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var text: TextView
+    private lateinit var image: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val butterflies = listOf(
-            Butterfly("Голубянка", R.drawable.but, 200000),
-            Butterfly("Пестрокрыльнича", R.drawable.but1, 345000),
-            Butterfly("Павлиний глаз", R.drawable.but3, 345620),
-            Butterfly("Moнаpx", R.drawable.but4, 2341567),
-            Butterfly("Адмирал", R.drawable.but5, 8765400)
-        )
-        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-        val adapter: ButterflyAdapter = ButterflyAdapter(this, butterflies)
-        recyclerView.layoutManager = LinearLayoutManager(this);
-        recyclerView.adapter = adapter
+        text = findViewById(R.id.txt)
+        image = findViewById(R.id.img)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        val searchItem: MenuItem = menu!!.findItem(R.id.action_pig1)
+        val searchView: EditText = MenuItemCompat.getActionView(searchItem) as EditText
+        searchView.hint = "Набирай что-нибудь"
+        searchView.setOnFocusChangeListener { v, hasFocus -> text.text = searchView.text }
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_settings -> {
+                text.text = "Что-то настраивается"
+                return true
+            }
+
+            R.id.action_pig1 -> {
+                text.text = ""
+                image.setImageResource(R.drawable.but)
+                return true
+            }
+
+            R.id.action_pig2 -> {
+                text.text = "Хряк"
+                image.setImageResource(R.drawable.but1)
+                return true
+            }
+
+            R.id.action_pig3 -> {
+                text.text = "Боров"
+                image.setImageResource(R.drawable.but2)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
